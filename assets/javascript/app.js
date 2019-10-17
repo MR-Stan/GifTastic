f71a13bd830717097370684e219ac7ed8ab80a0e
MR-Stan <michaelrstanley87@gmail.com>
2019-10-16 19:03:35 -0500

    making progress but still not working :(

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,21 @@
 let gameObject = {
 
     // array of searches, buttons created based on content of this, stored in localStorage
-    itemArray : ["Test"],
+    itemArray : [],
 
     // current search
-    search : "",
+    currentSearch : "",
 
     // Giphy API key
     key : "BImvRhJyYtNtABPNHp8mKOJB36yjcfK2",
 
     // number of gifs display on screen after button click
-    resultNumber : 10, // returns 10 results
+    limit : 10, // returns 10 results
 
     // connects to giphy to obtain gifs
-    queryURL : "https://api.giphy.com/v1/gifs/search?q=${this.search}&api_key=${this.key}&limit=${this.resultNumber}",
+    //queryURL : "https://api.giphy.com/v1/gifs/search?q=${this.search}&api_key=${this.key}&limit=${this.limit}",
+
+    queryURL : "https://api.giphy.com/v1/gifs/search?api_key=BImvRhJyYtNtABPNHp8mKOJB36yjcfK2&q=&limit=25&offset=0&rating=G&lang=en",
 
     // check localStorage and create intial HTML elements
     initialize : function() {
@@ -25,38 +27,37 @@ let gameObject = {
 
             $("<p/>").text("Search: ").appendTo("#searchForm");
 
-            $("<textarea/>").attr("id", "searchInput").appendTo("#searchForm");
+            $("<input/>").attr("id", "searchInput").appendTo("#searchForm");
 
-            $("<button>").attr("id", "searchButton").text("Search").appendTo("#searchForm");
+            $("<button type='button'>").attr("id", "searchButton").text("Search").appendTo("#searchForm");
 
         $("<div/>").attr("id", "buttonContainer").appendTo("main");
+ 
+        this.newButton();
     },
 
-    // create buttons based on contents of itemArray
+    // // create buttons based on contents of itemArray
     arrayButtons : function() {
         // clear buttonContainer to prevent duplicates
         $("#buttonContainer").empty();
         // create a button for each item in itemArray
         this.itemArray.forEach(function(i) {
-            console.log(i);
             $("<button>").attr("data-name", i).text(i).appendTo("#buttonContainer");
         });
     },
 
+    // when a button is clicked
     liveButton : function() {
         // creates AJAX call for the specific search
         $.ajax({
             url: gameObject.queryURL,
             method: "GET"
             }).then(function(response) {
-            let results = response.data;
-            console.log(results);
-            });
-    },
+                console.log(response);
+                let results = response.data;
+                console.log(results);
 
-    search : function(result) {
-        this.search = result;
-        this.liveButton();
+            });
     },
 
     newButton : function() {
@@ -64,46 +65,53 @@ let gameObject = {
         $("#searchButton").on("click", function(event) {
             // prevents something...
             event.preventDefault;
-            let currentSearch = $("#searchInput").val().trim();
-            console.log(currentSearch);
+            // taking in user input
+            gameObject.currentSearch = $("#searchInput").val().trim();
             // may need to update for random characters
-            if (currentSearch !== "") {
-                gameObject.itemArray.push(currentSearch);
-                console.log(gameObject.itemArray);
-                //localStorage.clear();
-                //localStorage.setItem("arrayItem", JSON.stringify(itemArray));
-            gameObject.search(currentSearch);
+            // as long as currentSearch isn't blank do...
+            if (gameObject.currentSearch !== "") {
+                // add the user's entry to itemArray and...
+                gameObject.itemArray.push(gameObject.currentSearch);
+                // localStorage for next time
+                // localStorage.clear();
+                // localStorage.setItem("arrayItem", JSON.stringify(itemArray));
             gameObject.arrayButtons();
             $("#searchInput").val("");
+            gameObject.liveButton();
             }
         });
     },
 
-    testGifStatus : function() {
-        // create a class 
-        $(document).on("click", "class goes here", function() {
-            let gifState = $(this).attr("data-state");
-            console.log(gifState);
-            if (gifState === "animate") {
-                gifStop();
-            }
-            else if (gifState === "still") {
-                gifGo();
-            }
-        });
-    },
+    // // check if a gif is moving or still
+    // testGifStatus : function() {
+    //     // create a class 
+    //     $(document).on("click", "class goes here", function() {
+    //         let gifState = $(this).attr("data-state");
+    //         console.log(gifState);
+    //         if (gifState === "animate") {
+    //             gifStop();
+    //         }
+    //         else if (gifState === "still") {
+    //             gifGo();
+    //         }
+    //     });
+    // },
 
-    gifStop : function() {
-        $(this).attr("data-state", "still")
-        // css effects
-    },
+    // // make a moving gif stop
+    // gifStop : function() {
+    //     $(this).attr("data-state", "still")
+    //     // css effects
+    // },
 
-    gifGo : function() {
-        $(this).attr("data-state", "animate")
-        // css effects
-    },
+    // // make a still gif move
+    // gifGo : function() {
+    //     $(this).attr("data-state", "animate")
+    //     // css effects
+    // },
 
 }
 
 gameObject.initialize();
-gameObject.arrayButtons();
+//
+
+
